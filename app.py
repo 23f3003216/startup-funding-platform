82d19d93c2f0941a1ef273e9efae504b4472c838
Hiya Syal <23f3003216@ds.study.iitm.ac.in>
2024-06-15 16:01:49 +0530

    add.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+import config
+db = SQLAlchemy(app)
+
+import routes
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
